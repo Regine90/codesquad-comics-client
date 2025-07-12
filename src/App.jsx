d4ba510843b0160323310ad04d2fca98366aba4f
Regine Valmont <sarahsmithregine90@gmail.com>
2025-07-11 21:17:47 -0400

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Admin from "./components/Admin";
+import Create from "./components/Create";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Update from "./components/Update";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,24 +34,29 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <div className="page-wrapper">
-          <Header user={user} setUser={setUser} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup user={user} setUser={setUser} />} />
-            <Route path="/update/:id" element={<Update />} />
-          </Routes>
-          <Footer />
+          <Router>
+            <Header user={user} setUser={setUser} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/signup"
+                element={<Signup user={user} setUser={setUser} />}
+              />
+              <Route path="/update/:id" element={<Update />} />
+            </Routes>
+            <Footer />
+          </Router>
         </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
